0b6ccca1441231907d986f3fb16f28ad401408f7
Slaton <slsp8752@colorado.edu>
2017-02-06 16:16:17 -0700

    Started dynamic text

--- mover.js
+++ mover.js
@@ -2,11 +2,33 @@ var img = new Image();
 img.src = "dog.jpg"; 
 var canvas = new fabric.Canvas("canvas");
 
+var x = document.createElement("INPUT");
+x.setAttribute("type", "text");
+x.setAttribute("value", "Hello World!");
+x.setAttribute("id", "majorInp");
+x.setAttribute("onkeyup", "updateText(this);");
+
+
+
+document.body.appendChild(x);
+//TODO update canvas text
+function updateText(inputElement)
+{
+    console.log(inputElement.value);
+	Bar.setText("my_text");
+	canvas.renderAll();
+
+}
+
+
 function render(){
 	canvas.setWidth(img.width);
 	canvas.setHeight(img.height);
 	canvas.renderAll();
 }
+//TODO: Make rectangles sized based on text input
+//		Draw text
+//		Move text with rectangle
 
 img.onload = function(){
 	canvas.setBackgroundImage("dog.jpg", render);
@@ -23,12 +45,15 @@ img.onload = function(){
 	  stroke: "red"
 	});
 
-	var movingBox = new fabric.Rect({
-	  width: 100,
-	  height: 100,
-	  hasBorders: false,
-	  hasControls: false,
-	  fill: "white"
+	var movingBox = new fabric.Text(document.getElementById("majorInp").value,{//fabric.Rect({
+		backgroundColor: "white",
+		hasBorders: false,
+		hasControls: false
+	  //width: 100,
+	  //height: 100,
+	  //hasBorders: false,
+	  //hasControls: false,
+	  //fill: "white"
 	});
 
 	canvas.on("object:moving", function() {
@@ -44,7 +69,7 @@ img.onload = function(){
 
 	  movingBox.setLeft(Math.min(Math.max(left, leftBound), rightBound - movingBox.width));
 	  movingBox.setTop(Math.min(Math.max(top, topBound), bottomBound - movingBox.height));
-	});
+});
 
 	canvas.add(boundingBox);
 	canvas.add(movingBox);
